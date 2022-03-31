e052768a9b57b93212e88b15c2cd52ec10a24cee
KIJUN24 <97867568+KIJUN24@users.noreply.github.com>
2022-03-31 20:31:32 +0900

    12 commit

--- GameMake/GM_7/code/level.py
+++ GameMake/GM_7/code/level.py
@@ -1,7 +1,7 @@
 import pygame
 from support import import_csv_layout, import_cut_graphics
 from settings import tile_size
-from tiles import Tile, StaticTile, Crate
+from tiles import Tile, StaticTile, Crate, Coin, Palm
 
 class Level:
     def __init__(self, level_data, surface):
@@ -21,6 +21,17 @@ class Level:
         crate_layout = import_csv_layout(level_data['crates'])
         self.crate_sprites = self.create_tile_group(crate_layout, 'crates')
 
+        # coins
+        coin_layout = import_csv_layout(level_data['coins'])
+        self.coin_sprites = self.create_tile_group(coin_layout, 'coins')
+
+        # foreground palms
+        fg_palm_layout = import_csv_layout(level_data['fg palms'])
+        self.fg_palm_sprites = self.create_tile_group(fg_palm_layout, 'fg palms')
+
+        # background palms
+        bg_palm_layout = import_csv_layout(level_data['bg palms'])
+        self.bg_palm_sprites = self.create_tile_group(bg_palm_layout, 'bg palms')
 
     def create_tile_group(self, layout, type):
         sprite_group = pygame.sprite.Group()
@@ -42,6 +53,21 @@ class Level:
                     if type == 'crates':
                         sprite = Crate(tile_size, x, y)
 
+                    if type == 'coins':
+                        if val == '0':
+                            sprite = Coin(tile_size, x, y, 'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\graphics\\coins\\gold')
+                        if val == '1':
+                            sprite = Coin(tile_size, x, y, 'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\graphics\\coins\\silver')
+                    
+                    if type == 'fg palms':
+                        if val == '0':
+                            sprite = Palm(tile_size, x, y, 'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\graphics\\terrain\\palm_small', 38)
+                        if val == '1':
+                            sprite = Palm(tile_size, x, y, 'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\graphics\\terrain\\palm_large', 64)
+
+                    if type == 'bg palms':
+                        sprite = Palm(tile_size, x, y, 'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\graphics\\terrain\\palm_bg', 64)
+
                     sprite_group.add(sprite)
 
         return sprite_group
@@ -60,3 +86,15 @@ class Level:
         # crates
         self.crate_sprites.update(self.world_shift)
         self.crate_sprites.draw(self.display_surface)
+
+        # coins
+        self.coin_sprites.update(self.world_shift)
+        self.coin_sprites.draw(self.display_surface)
+
+        # foreground palms
+        self.fg_palm_sprites.update(self.world_shift)
+        self.fg_palm_sprites.draw(self.display_surface)
+
+        # background palms
+        self.bg_palm_sprites.update(self.world_shift)
+        self.bg_palm_sprites.draw(self.display_surface)
