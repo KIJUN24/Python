64397d53ff17a55ecc1e3f8493082e7a197be02e
KIJUN24 <97867568+KIJUN24@users.noreply.github.com>
2022-04-07 22:41:57 +0900

    15 commit

---
+++ GameMake/GM_8_7/code/game_data.py
@@ -0,0 +1,85 @@
+level_0 = {
+    'terrain':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._terrain.csv',
+    'coins':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._coins.csv',
+    'fg palms':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._fg_palms.csv',
+    'bg palms':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._gb_palms.csv',
+    'crates':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._crates.csv',
+    'enemies':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._enemies.csv',
+    'constraints':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._constraints.csv',
+    'player':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._player.csv',
+    'grass':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._grass.csv',
+    'node_pos' : (100, 400),
+    'unlock' : 1,
+    'node_graphics' : 'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_8_7\\graphics\\overworld\\0'}
+
+            ######### tile부터 만들기 #########
+
+level_1 = {
+    'terrain':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._terrain.csv',
+    'coins':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._coins.csv',
+    'fg palms':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._fg_palms.csv',
+    'bg palms':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._gb_palms.csv',
+    'crates':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._crates.csv',
+    'enemies':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._enemies.csv',
+    'constraints':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._constraints.csv',
+    'player':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._player.csv',
+    'grass':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._grass.csv',
+    'node_pos' : (300, 220),
+    'unlock' : 2,
+    'node_graphics' : 'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_8_7\\graphics\\overworld\\1'}
+
+level_2 = {
+    'terrain':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._terrain.csv',
+    'coins':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._coins.csv',
+    'fg palms':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._fg_palms.csv',
+    'bg palms':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._gb_palms.csv',
+    'crates':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._crates.csv',
+    'enemies':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._enemies.csv',
+    'constraints':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._constraints.csv',
+    'player':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._player.csv',
+    'grass':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._grass.csv',
+    'node_pos' : (480, 610),
+    'unlock' : 3,
+    'node_graphics' : 'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_8_7\\graphics\\overworld\\2'}
+
+level_3 = {
+    'terrain':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._terrain.csv',
+    'coins':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._coins.csv',
+    'fg palms':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._fg_palms.csv',
+    'bg palms':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._gb_palms.csv',
+    'crates':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._crates.csv',
+    'enemies':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._enemies.csv',
+    'constraints':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._constraints.csv',
+    'player':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._player.csv',
+    'grass':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._grass.csv',
+    'node_pos' : (610, 350),
+    'unlock' : 4,
+    'node_graphics' : 'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_8_7\\graphics\\overworld\\3'}
+
+level_4 = {
+    'terrain':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._terrain.csv',
+    'coins':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._coins.csv',
+    'fg palms':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._fg_palms.csv',
+    'bg palms':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._gb_palms.csv',
+    'crates':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._crates.csv',
+    'enemies':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._enemies.csv',
+    'constraints':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._constraints.csv',
+    'player':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._player.csv',
+    'grass':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._grass.csv',
+    'node_pos' : (880, 210),
+    'unlock' : 5,
+    'node_graphics' : 'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_8_7\\graphics\\overworld\\4'}
+
+level_5 = {
+    'terrain':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._terrain.csv',
+    'coins':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._coins.csv',
+    'fg palms':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._fg_palms.csv',
+    'bg palms':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._gb_palms.csv',
+    'crates':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._crates.csv',
+    'enemies':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._enemies.csv',
+    'constraints':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._constraints.csv',
+    'player':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._player.csv',
+    'grass':'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_7\\Levels\\0\\level_0._grass.csv',
+    'node_pos' : (1050, 400),
+    'unlock' : 5,
+    'node_graphics' : 'C:\\Users\\lkjun\\OneDrive\\바탕 화면\\PythonWorkspace\\python_study\\Python_practice\\GameMake\\GM_8_7\\graphics\\overworld\\5'}
