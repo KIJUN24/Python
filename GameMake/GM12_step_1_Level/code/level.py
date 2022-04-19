e6277351e179db5de556b08d75657ce62216c9a3
KIJUN24 <97867568+KIJUN24@users.noreply.github.com>
2022-04-20 00:59:03 +0900

    24 commit

--- GameMake/GM12_step_1_Level/code/level.py
+++ GameMake/GM12_step_1_Level/code/level.py
@@ -1,4 +1,5 @@
 import pygame
+from magic import MagicPlayer
 from enemy import Enemy
 from settings import *
 from tile import Tile
@@ -9,12 +10,14 @@ from random import choice, randint
 from weapon import Weapon
 from ui import UI
 from particles import AnimationPlayer
+from upgrade import Upgrade
 
 class Level:
     def __init__(self):
 
         # get the display surface
         self.display_surface = pygame.display.get_surface()
+        self.game_paused = False
 
         # sprite group setup
         self.visible_sprites = YSortCameraGroup()
@@ -30,9 +33,11 @@ class Level:
 
         # user interface
         self.ui = UI()
+        self.upgrade = Upgrade(self.player)
 
         # particels
         self.animation_player = AnimationPlayer()
+        self.magic_player = MagicPlayer(self.animation_player)
 
     def create_map(self):
         layouts = {
@@ -91,15 +96,18 @@ class Level:
                                     [self.visible_sprites, self.attackable_sprites], 
                                     self.obstacle_sprites,
                                     self.damage_player,
-                                    self.trigger_death_particles)
+                                    self.trigger_death_particles,
+                                    self.add_xp)
 
     def create_attack(self):
         self.current_attack = Weapon(self.player, [self.visible_sprites, self.attack_sprites])
 
     def create_magic(self, style, strength, cost):
-        print(style)
-        print(strength)
-        print(cost)
+        if style == 'heal':
+            self.magic_player.heal(self.player, strength, cost, [self.visible_sprites])
+
+        if style == 'flame':
+            self.magic_player.flame(self.player, cost, [self.visible_sprites, self.attack_sprites])
 
     def destory_attack(self):
         if self.current_attack:
@@ -131,13 +139,24 @@ class Level:
     def trigger_death_particles(self, pos, particles_type):
         self.animation_player.create_particles(particles_type, pos, self.visible_sprites)
 
+    def add_xp(self, amount):
+        self.player.exp += amount
+
+    def toggle_menu(self):
+        self.game_paused = not self.game_paused
+
     def run(self):
-        # update and draw the game
         self.visible_sprites.custom_draw(self.player)
-        self.visible_sprites.update()
-        self.visible_sprites.enemy_update(self.player)
-        self.player_attack_logic()
         self.ui.display(self.player)
+
+        if self.game_paused:
+            # display upgrade menu
+            self.upgrade.display()
+        else:
+            # run the game
+            self.visible_sprites.update()
+            self.visible_sprites.enemy_update(self.player)
+            self.player_attack_logic()
 
 class YSortCameraGroup(pygame.sprite.Group):
     def __init__(self):
--- GameMake/GM12_step_1_Level/code/main.py
+++ GameMake/GM12_step_1_Level/code/main.py
@@ -13,15 +13,22 @@ class Game:
         
         self.level = Level()
 
+        # sound
+        main_sound = pygame.mixer.Sound('GameMake/GM12_step_1_Level/audio/main.ogg')
+        main_sound.play(loops = -1)
+
     def run(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_m:
+                        self.level.toggle_menu()
             
-            self.screen.fill('black')
-            self.level.run( )
+            self.screen.fill(WATER_COLOR)
+            self.level.run()
             pygame.display.update()
             self.clock.tick(FPS)
 
