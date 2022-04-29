3c90490fab893e0f1cf9e356a40963185293c5f2
KIJUN24 <97867568+KIJUN24@users.noreply.github.com>
2022-04-29 20:46:22 +0900

    29 commit

---
+++ GameMake/GM_16_self_review/data/tsx/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="tiles" tilewidth="128" tileheight="128" tilecount="299" columns="23">
+ <image source="../../../../../../../Tiled-main/Tiled-main/graphics/tilesets/tiles.png" width="2944" height="1664"/>
+</tileset>
