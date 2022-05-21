9f8c4ca58f508a3bb71745c5b3f239db1c119896
KIJUN24 <97867568+KIJUN24@users.noreply.github.com>
2022-05-21 23:44:49 +0900

    45 commit

---
+++ GameMake/GM_test/code/level.py
@@ -0,0 +1,11 @@
+import pygame
+
+class Level:
+    def __init__(self):
+        self.display_surface = pygame.display.get_surface()
+
+    def create_image(self):
+        pass
+
+    def run(self):
+        pass
