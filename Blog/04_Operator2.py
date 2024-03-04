b68cbe891a7b669e5cb6e38d390747f579d75dc7
Kijun Lee <lkjun0319456@naver.com>
2024-03-04 13:42:10 +0900

    Blog Python

---
+++ Blog/04_Operator2.py
@@ -0,0 +1,26 @@
+a = 10
+b = 20
+c = 10
+d = 5
+
+print(a == b)
+print(a == c)
+print()
+print(a != b)
+print(a != c)
+print()
+print(a > b)
+print(a > c)
+print(a > d)
+print()
+print(a < b)
+print(a < c)
+print(a < d)
+print()
+print(a >= b)
+print(a >= c)
+print(a >= d)
+print()
+print(a <= b)
+print(a <= c)
+print(a <= d)
